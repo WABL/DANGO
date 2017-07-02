c5d4a0815be1098f140663bcf2a9899d9e4d04b6
CMK <cirno.mainasuk@gmail.com>
2017-07-02 17:48:39 +0800

    feat: add DemoController:demo test

--- src/io/dango/test/DemoControllerTest.java
+++ src/io/dango/test/DemoControllerTest.java
@@ -1,40 +1,85 @@
 package io.dango.test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
 /**
  * Created by 54472 on 2017/6/30.
  */
 public class DemoControllerTest {
-    @org.junit.Test
-    public void demo() throws Exception {
 
+    HttpClient httpClient;
+
+    @Before
+    public void before() {
+        HttpClient httpClient = new HttpClient();
     }
 
-    @org.junit.Test
+    @After
+    public void after() {
+        httpClient = null;
+    }
+
+    // Use HttpClient Get test
+    @Test
+    public void demo() throws Exception {
+        HttpMethod method = new GetMethod("http://localhost:8080/demo");
+        int statusCode = httpClient.executeMethod(method);
+
+        // Assert HTTP request return OK
+        Assert.assertEquals(HttpStatus.OK.value(), statusCode);
+
+        byte[] responseBody = method.getResponseBody();
+
+        // Assert HTTP response body not null
+        Assert.assertNotNull(responseBody);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String body = new String(responseBody);
+        List<String> strList = mapper.readValue(body, new TypeReference<List<String>>() { });
+
+        // Assert JSON parse
+        Assert.assertEquals("Hello", strList.get(0));
+        Assert.assertEquals("World", strList.get(1));
+    }
+
+    @Test
     public void error() throws Exception {
 
     }
 
-    @org.junit.Test
+    // Use RestTemplate test via getForObject
+    @Test
     public void faceNotFound() throws Exception {
-
     }
 
-    @org.junit.Test
+    @Test
     public void faceByID() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void saveFace() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void saveFace2() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void saveFace3() throws Exception {
 
     }
