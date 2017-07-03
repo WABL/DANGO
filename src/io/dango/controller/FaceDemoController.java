e68df48d74c3bf5150211c0ae6bc5e6530339aa3
CMK <cirno.mainasuk@gmail.com>
2017-07-03 16:58:58 +0800

    feat: add face detect client & finish detect API of server side

--- src/io/dango/controller/FaceDemoController.java
+++ src/io/dango/controller/FaceDemoController.java
@@ -1,10 +1,16 @@
 package io.dango.controller;
 
+import io.dango.utility.FaceDetechTool;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 /**
  * Created by MainasuK on 2017-7-3.
@@ -14,6 +20,11 @@ public class FaceDemoController {
 
     @RequestMapping(path = "/face/detect", method = RequestMethod.POST, produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] detect(@RequestParam("photo") MultipartFile photo) throws IOException {
-        return photo.getBytes();
+        InputStream in = new ByteArrayInputStream(photo.getBytes());
+        BufferedImage image = new FaceDetechTool().detechFace(ImageIO.read(in));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ImageIO.write(image, ".jpg",out);
+        return out.toByteArray();
     }
 }
