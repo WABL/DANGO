31effb6e513990748c9ec5d5df2203b02861a615
DESKTOP-SJU2OCJ\54472 <544722253@qq.com>
2017-09-04 15:06:13 +0800

    no message

--- src/io/dango/controller/LoginControllerTest.java
+++ src/io/dango/controller/LoginControllerTest.java
@@ -1,17 +1,19 @@
 package io.dango.controller;
 
-import org.apache.oltu.oauth2.client.OAuthClient;
-import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
-import org.apache.oltu.oauth2.client.response.GitHubTokenResponse;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.json.JSONObject;
 import org.junit.Test;
 
-import java.net.URI;
-
-import static org.junit.Assert.*;
+import java.io.IOException;
 
 public class LoginControllerTest {
+    String URL = "http://localhost:8080";
 
     @Test
     public void login() throws Exception {
@@ -33,4 +35,28 @@ public class LoginControllerTest {
         System.out.println(request.getLocationUri());
     }
 
+    @Test
+    public void loginToken() throws IOException {
+        String url = URL+"/oauth/token";
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        Content content = Request.Post(url)
+                .addHeader("Accept","application/json")
+                .addHeader("Authorization","Basic YW5kcm9pZDpxaWRpYW4=")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+                .bodyForm(
+                        Form.form()
+                                .add("password", "USER")
+                                .add("response_type", "code")
+                                .add("scope", "read write")
+                                .add("grant_type", "password")
+                                .add("username", "user")
+                                .add("client_secret", "qidian")
+                                .add("client_id", "android")
+                                .build())
+                .execute().returnContent();
+
+        System.out.println(content.toString());
+        JSONObject jsonObject = new JSONObject(content.toString());
+        String token = jsonObject.getString("access_token");
+    }
 }
